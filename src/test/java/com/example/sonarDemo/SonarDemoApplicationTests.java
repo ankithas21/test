dd589e5d7042c0d340d49fc262a01c54f035929e
ankitha.s <ankitha.s@sclera.com>
2024-08-06 17:20:47 +0530

    first commit

---
+++ src/test/java/com/example/sonarDemo/SonarDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sonarDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonarDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
